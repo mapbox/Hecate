c3e92294ed83798e3ca76e8b8891a555e0513bff
ingalls <ingalls@protonmail.com>
2017-10-27 23:00:15 -0400

    Sketch out XML proto

--- src/xml/mod.rs
+++ src/xml/mod.rs
@@ -1,7 +1,13 @@
 extern crate geojson;
+extern crate quick_xml;
+
+use std::io::Cursor;
+use self::quick_xml::writer::Writer;
+use self::quick_xml::events as XMLEvents;
 
 pub enum XMLError {
-    Unknown
+    Unknown,
+    GCNotSupported
 }
 
 impl XMLError {
@@ -9,13 +15,66 @@ impl XMLError {
         match &self {
             Unknown => {
                 "Unknown Error"
+            },
+            GCNotSupported => {
+                "GeometryCollection are not currently supported"
             }
         }
     }
 }
 
-pub fn from(geojson: &geojson::FeatureCollection) -> Result<String, XMLError> {
+struct OSMTypes {
+    nodes: String,
+    ways: String,
+    rels: String
+}
 
-    let test_str = String::from("test");
-    Ok(test_str)
+impl OSMTypes {
+    pub fn new() -> OSMTypes {
+        OSMTypes {
+            nodes: String::from(""),
+            ways: String::from(""),
+            rels: String::from("")
+        }
+    }
+}
+
+pub fn from(fc: &geojson::FeatureCollection) -> Result<String, XMLError> {
+    let mut xml: String = String::from(r#"<?xml version='1.0' encoding='UTF-8'?><osm version="0.6" generator="ROSM">"#);
+    let mut osm = OSMTypes::new();
+
+    for feat in &fc.features {
+        match feat.geometry {
+            Some(geom) => {
+                match geom {
+                    geojson::Value::Point => point(&feat, &osm),
+                    //MultiPoint(mpt) => multipoint(&feat, &mpt, &osm),
+                    //LineString(ln) => linestring(&feat, &ln, &osm),
+                    //MultiLineString(mln) => multilinestring(&feat, &mln, &osm),
+                    //Polygon(py) => polygon(&feat, &py, &osm),
+                    //MultiPolygon(mpy) => multipolygon(&feat, &mpy, &osm),
+                    _ => { return Err(XMLError::GCNotSupported) },
+                }
+            }
+            None => ()
+        }
+    }
+
+    xml.push_str("</osm>");
+
+    Ok(xml)
+}
+
+pub fn point(feat: &geojson::Feature, osm: &OSMTypes) {
+}
+
+pub fn multipoint(feat: &geojson::Feature, osm: &OSMTypes) {
+}
+pub fn linestring(feat: &geojson::Feature, osm: &OSMTypes) {
+}
+pub fn multilinestring(feat: &geojson::Feature, osm: &OSMTypes) {
+}
+pub fn polygon(feat: &geojson::Feature, osm: &OSMTypes) {
+}
+pub fn multipolygon(feat: &geojson::Feature, osm: &OSMTypes) {
 }
