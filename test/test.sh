407188e11da880f98ecc541c23addef0ce10e0b5
ingalls <ingalls@protonmail.com>
2018-01-17 14:55:45 -0500

    Point to test.sh

---
+++ test/test.sh
@@ -0,0 +1,33 @@
+set -euo pipefail
+
+service postgresql start
+
+cd $(dirname $0)/..
+cargo build
+
+killall -9 hecate || true
+
+function startsrv() {
+    cargo run&
+
+    sleep 2
+
+    # On OSX when cargo first runs it will be connected to `localhost:8000`
+    # but not to 127.0.0.1. curl localhost:8000  will work but curl 127.0.0.1 will fail.
+    # Node request converts localhost:8000 to 127.0.0.1 behind the scenes, resulting in failure
+    # running a second startsrc fixes this.
+    if ! curl '127.0.0.1:8000'; then
+        startsrv
+    else
+        testsrv
+    fi
+}
+
+function testsrv() {
+    for TEST in tests/; do
+        node tests/$TEST
+    done
+
+}
+
+startsrv
