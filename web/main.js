8a80d5b0aeed57b3b0b0901b0a10ca021df440c4
samely <eliane@mapbox.com>
2018-03-20 14:02:12 -0400

    Table rendering for street property

--- web/main.js
+++ web/main.js
@@ -292,6 +292,7 @@ window.onload = () => {
         container: 'map',
         style: 'mapbox://styles/mapbox/satellite-v9',
         center: [-96, 37.8],
+        hash: true,
         maxzoom: 14,
         zoom: 3
     });
