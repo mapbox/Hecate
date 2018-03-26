ce051d12061d1947c4d8872d4abfecf84720c139
ingalls <ingalls@protonmail.com>
2018-03-26 11:04:16 -0400

    Add tile tests

---
+++ tests/tiles.rs
@@ -0,0 +1,79 @@
+extern crate reqwest;
+extern crate postgres;
+extern crate hecate;
+
+#[cfg(test)]
+mod test {
+    use std::fs::File;
+    use std::io::prelude::*;
+    use postgres::{Connection, TlsMode};
+    use std::process::Command;
+    use std::time::Duration;
+    use std::thread;
+    use reqwest;
+
+    #[test]
+    fn tiles() {
+        { // Reset Database:
+            let conn = Connection::connect("postgres://postgres@localhost:5432", TlsMode::None).unwrap();
+
+            conn.execute("
+                SELECT pg_terminate_backend(pg_stat_activity.pid)
+                FROM pg_stat_activity
+                WHERE
+                    pg_stat_activity.datname = 'hecate'
+                    AND pid <> pg_backend_pid();
+            ", &[]).unwrap();
+
+            conn.execute("DROP DATABASE IF EXISTS hecate;", &[]).unwrap();
+            conn.execute("CREATE DATABASE hecate;", &[]).unwrap();
+
+            let conn = Connection::connect("postgres://postgres@localhost:5432/hecate", TlsMode::None).unwrap();
+
+            let mut file = File::open("./src/schema.sql").unwrap();
+            let mut table_sql = String::new();
+            file.read_to_string(&mut table_sql).unwrap();
+            conn.batch_execute(&*table_sql).unwrap();
+        }
+
+        let mut server = Command::new("cargo").arg("run").spawn().unwrap();
+        thread::sleep(Duration::from_secs(1));
+
+        { //Request a tile via API
+            let client = reqwest::Client::new();
+            let mut resp = client.get("http://localhost:8000/api/tiles/1/2/3").send().unwrap();
+
+            let mut body: Vec<u8> = Vec::new();
+            resp.read_to_end(&mut body).unwrap();
+
+            assert_eq!(body.len(), 13);
+            assert!(resp.status().is_success());
+        }
+
+        { //Ensure Tile was places in DB tilecache
+            let conn = Connection::connect("postgres://postgres@localhost:5432/hecate", TlsMode::None).unwrap();
+            let res = conn.query("SELECT ref, tile FROM tiles", &[]).unwrap();
+
+            assert_eq!(res.len(), 1);
+
+            let tile_ref: String = res.get(0).get(0);
+            assert_eq!(tile_ref, String::from("1/2/3"));
+
+            let tile: Vec<u8> = res.get(0).get(1);
+            assert_eq!(tile.len(), 10);
+        }
+
+        { //Request a tile via API
+            let client = reqwest::Client::new();
+            let mut resp = client.get("http://localhost:8000/api/tiles/1/2/3").send().unwrap();
+
+            let mut body: Vec<u8> = Vec::new();
+            resp.read_to_end(&mut body).unwrap();
+
+            assert_eq!(body.len(), 13);
+            assert!(resp.status().is_success());
+        }
+
+        server.kill().unwrap();
+    }
+}
