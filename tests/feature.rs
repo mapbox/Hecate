35ceaa2e88ff0af1d5e2bc084060f0a96d2d88e2
ingalls <ingalls@protonmail.com>
2018-05-16 08:04:03 -0400

    Add features check

--- tests/feature.rs
+++ tests/feature.rs
@@ -247,6 +247,12 @@ mod test {
             //TODO check body
         }
 
+        {
+            let resp = reqwest::get("http://localhost:8000/api/data/features?bbox=-107.578125,-30.600094,56.162109,46.377254").unwrap();
+            assert!(resp.status().is_success());
+            //TODO check body
+        }
+
         { //Modify Point
             let client = reqwest::Client::new();
             let mut resp = client.post("http://localhost:8000/api/data/feature")
