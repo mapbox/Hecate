9a77d09338cd5586b605005d85114ff72120d0fe
ingalls <ingalls@protonmail.com>
2018-06-06 11:11:27 -0400

    Add query endpoint

--- src/clone/mod.rs
+++ src/clone/mod.rs
@@ -42,29 +42,16 @@ pub fn get(conn: r2d2::PooledConnection<r2d2_postgres::PostgresConnectionManager
 }
 
 pub fn query(read_conn: r2d2::PooledConnection<r2d2_postgres::PostgresConnectionManager>, query: &String) -> Result<PGStream, CloneError> {
-    /*
-    match PGStream::new(conn, String::from("next_clone_query"), format!(r#"
-
-        DECLARE next_clone CURSOR FOR
+    match PGStream::new(read_conn, String::from("next_clone_query"), format!(r#"
+        DECLARE next_clone_query CURSOR FOR
             SELECT
                 row_to_json(t)::TEXT
             FROM (
-                SELECT
-                    geo.id AS id,
-                    geo.key AS key,
-                    'Feature' AS type,
-                    geo.version AS version,
-                    ST_AsGeoJSON(geo.geom)::JSON AS geometry,
-                    geo.props AS properties
-                FROM
-                    geo
+                {}
             ) t
 
-    "#), &[]) {
+    "#, query), &[]) {
         Ok(stream) => Ok(stream),
         Err(_) =>  Err(CloneError::GetError)
     }
-    */
-
-    Err(CloneError::GetError)
 }
