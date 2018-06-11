691db658d75d38a7c5daf6f04ea21072e0f7e8b8
ingalls <ingalls@protonmail.com>
2018-06-11 12:28:11 -0400

    Cleaner headers

--- web/src/panels/Deltas.vue
+++ web/src/panels/Deltas.vue
@@ -2,14 +2,13 @@
 <div class='flex-parent flex-parent--column viewport-third h-auto-ml hmax-full bg-white round-ml shadow-darken10' style="pointer-events:auto;">
     <template v-if='delta'>
         <div class='flex-child px12 py12'>
-            <h3 class='fl py6 txt-m txt-bold'>Hecate Delta</h3>
+            <h3 class='fl py6 txt-m txt-bold'>Delta #<span v-text="delta.id"></span></h3>
+            <button @click="delta = false" class='fr btn round bg-gray-light bg-darken25-on-hover color-gray-dark fl'><svg class='icon'><use href='#icon-arrow-left'/></button>
         </div>
 
-        <div class='flex-child clearfix px12 py12 bg-gray-faint round-b-ml txt-s'>
-            <button @click="delta = false" class='btn round bg-gray-light bg-darken25-on-hover color-gray-dark fl'><svg class='icon'><use href='#icon-arrow-left'/></button>
-            <div class="px12 py3 txt-bold fl">Delta <span v-text="delta.id"></span></div>
+        <div class="flex-child py12 px12 bg-gray-faint txt-s align-center">
+            <span v-text="delta.props.message ? delta.props.message : '<No Delta Message>'"></span>
         </div>
-        <div class="flex-child py12 px12 bg-gray-faint txt-s align-center"><span v-text="delta.props.message ? delta.props.message : '<No Delta Message>'"></span></div>
 
         <div class="flex-child scroll-auto">
             <div v-for="(feat, feat_it) in delta.features.features" class="px12 py3 clearfix bg-white bg-darken25-on-hover cursor-pointer">
@@ -28,7 +27,7 @@
     </template>
     <template v-else>
         <div class='flex-child px12 py12 border--gray-light border-b'>
-            <h3 class='fl py6 txt-m txt-bold'>Hecate Deltas</h3>
+            <h3 class='fl py6 txt-m txt-bold'>Deltas</h3>
             <button @click="getDeltas" class='btn round bg-gray-light bg-darken25-on-hover color-gray-dark fr'><svg class='icon'><use href='#icon-refresh'/></button>
         </div>
 
