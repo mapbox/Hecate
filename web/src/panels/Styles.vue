a31335ca8507a54506f22d0c559166a9573fda88
ingalls <ingalls@protonmail.com>
2018-06-16 12:33:17 -0400

    Get Styles

--- web/src/panels/Styles.vue
+++ web/src/panels/Styles.vue
@@ -2,7 +2,7 @@
 <div class='flex-parent flex-parent--column viewport-third h-auto-ml hmax-full bg-white round-ml shadow-darken10' style="pointer-events:auto;">
     <div class='flex-child px12 py12'>
         <h3 class='fl py6 txt-m txt-bold'>Styles</h3>
-        <button @click="styles_refresh()" class='btn round bg-gray-light bg-darken25-on-hover color-gray-dark fr'><svg class='icon'><use href='#icon-refresh'/></button>
+        <button @click="getStyles()" class='btn round bg-gray-light bg-darken25-on-hover color-gray-dark fr'><svg class='icon'><use href='#icon-refresh'/></button>
 
         <template v-if="credentials.authed">
             <button @click="style_create()" class='fr btn mx6 btn--s round align-center'>New</button>
