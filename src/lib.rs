5882ac76d0bcd22a6f8ccf3b14395b7f67912482
ingalls <ingalls@protonmail.com>
2018-08-29 14:07:25 -0400

    v0.43.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(plugin, custom_derive, custom_attribute, attr_literals)]
 #![plugin(rocket_codegen)]
 
-static VERSION: &'static str = "0.42.0";
+static VERSION: &'static str = "0.43.0";
 
 #[macro_use] extern crate serde_json;
 #[macro_use] extern crate serde_derive;
