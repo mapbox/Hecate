d70d781b9e4226d9a340ee5baabed0ce2dd0c632
ingalls <ingalls@protonmail.com>
2018-08-30 13:59:50 -0400

    Flesh out Server UI Concept

--- web/src/modals/Settings.vue
+++ web/src/modals/Settings.vue
@@ -3,17 +3,22 @@
     <div class='flex-parent flex-parent--center-main flex-parent--center-cross h-full' style="pointer-events: auto;">
         <div class="flex-child px12 py12 w600 h80 bg-white round-ml shadow-darken10">
             <div class='grid w-full'>
-                <div class='col col--8'>
+                <div class='col col--12'>
                     <h3 class='fl py6 txt-m txt-bold w-full'>Settings</h3>
-                    <p>There are currently no server settings</p>
+
+                    <h4 class='fl py6 txt-m w-full border--gray border-b'>Base Layers</h4>
+
+                    <h4 class='fl py6 txt-m w-full border--gray border-b'>Default Style</h4>
+
                 </div>
 
-                <div class='col col--4'>
-                    <button @click='close()' class='mt24 btn round w-full'>OK</button>
+                <div class='col col--12'>
+                    <button @click='close()' class='fr mt24 btn round w-full'>OK</button>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </template>
 
 <script>
