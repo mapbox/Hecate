32f00c377c0de02e2c9e3ad903e0e065968d1eeb
ingalls <ingalls@protonmail.com>
2018-10-03 13:40:59 -0400

    Allow adding new baseLayers

--- web/src/modals/Settings.vue
+++ web/src/modals/Settings.vue
@@ -4,9 +4,22 @@
         <div class="flex-child px12 py12 w600 h80 bg-white round-ml shadow-darken10">
             <div class='grid w-full'>
                 <div class='col col--12'>
-                    <h3 class='fl py6 txt-m txt-bold w-full'>Settings</h3>
+                    <h3 class='col col--12 fl py6 txt-m txt-bold'>Settings</h3>
 
                     <h4 class='fl py6 txt-m w-full border--gray border-b'>Base Layers</h4>
+
+                    <div class='col col--12 hmin120 hmax180 clearfix'>
+                        <template v-for='layer of layers'>
+                            <div class='h120 w120 fl round border border--gray-light border--gray-on-hover color-gray-light color-gray-on-hover my12 mx12 cursor-pointer'>
+                                <svg class='icon w-full h-full'><use href='#icon-paint'/></svg>
+                            </div>
+                        </template>
+
+                        <!-- Add a new Base Layer -->
+                        <div @click='newLayer' class='h120 w120 fl round border border--gray-light border--gray-on-hover color-gray-light color-gray-on-hover my12 mx12 cursor-pointer'>
+                            <svg class='icon w-full h-full'><use href='#icon-plus'/></svg>
+                        </div>
+                    </div>
 
                     <h4 class='fl py6 txt-m w-full border--gray border-b'>Default Style</h4>
 
@@ -25,11 +38,22 @@
 export default {
     name: 'settings',
     data: function() {
-        return { }
+        return {
+            layers: []    
+        }
     },
     methods: {
         close: function() {
             this.$emit('close');
+        },
+        newLayer: function() {
+            this.layers.push({
+                name: 'New Layer',
+                icon: '',
+                layer: {
+
+                }
+            });
         }
     },
     render: h => h(App),
