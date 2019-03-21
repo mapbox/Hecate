54a579d27ca69d8d512b49e9c31204aab82be4ab
ingalls <ingalls@protonmail.com>
2019-03-21 08:40:58 -0700

    Bundle Web

--- web/src/panels/Bounds.vue
+++ web/src/panels/Bounds.vue
@@ -3,19 +3,33 @@
     <div class='flex-child px12 py12'>
         <h3 class='fl py6 txt-m txt-bold'>Bounds</h3>
         <button @click="getBounds" class='btn round bg-gray-light bg-darken25-on-hover color-gray-dark fr'><svg class='icon'><use href='#icon-refresh'/></button>
+        <button @click="filterToggle = !filterToggle" class='btn round bg-gray-light bg-darken25-on-hover color-gray-dark fr mx3'><svg class='icon'><use href='#icon-search'/></button>
     </div>
 
     <div class="flex-child scroll-auto">
-        <div v-if="!bounds.length" class="px12 py3 clearfix bg-white">
-            <div align="center">No Boundaries</div>
+        <div v-if="filterToggle" class="relative px12 py3 clearfix bg-white">
+            <div class='absolute flex-parent flex-parent--center-cross flex-parent--center-main w36 h36'>
+                <svg class='icon'><use href='#icon-search'></use></svg>
+            </div>
+            <input class='input pl36' placeholder='Search' v-model="filter">
         </div>
 
-        <div v-for="(bound, bound_it) in bounds" class="w-full py3 clearfix bg-white bg-darken25-on-hover cursor-pointer">
-            <a v-bind:href="`/api/data/bounds/${bound}`" target="_blank" class="w-full clearfix">
-                <span class="fl py6 px6"><svg class='icon'><use href='#icon-database'/></span>
-                <span class="fl" v-text="bound"></span>
-            </a>
-        </div>
+        <template v-if='loading'>
+            <div class='flex-child loading h60 py12'></div>
+        </template>
+        <template v-else>
+            <div v-if="!bounds.length" class="px12 py3 clearfix bg-white">
+                <div align="center">No Boundaries</div>
+            </div>
+            <div v-else>
+                <div v-for="(bound, bound_it) in bounds" class="w-full py3 clearfix bg-white bg-darken25-on-hover cursor-pointer">
+                    <a v-bind:href="`/api/data/bounds/${bound}`" target="_blank" class="w-full clearfix">
+                        <span class="fl py6 px6"><svg class='icon'><use href='#icon-database'/></span>
+                        <span class="fl" v-text="bound"></span>
+                    </a>
+                </div>
+            </div>
+        </template>
     </div>
 
     <foot/>
@@ -29,24 +43,40 @@ export default {
     name: 'bounds',
     data: function() {
         return {
+            loading: true,
+            filter: '',
+            filterToggle: false,
             bounds: []
         }
     },
     created: function() {
         this.getBounds();
     },
+    watch: {
+        filter: function() {
+            this.getBounds();
+        }
+    },
     components: {
         foot: Foot
     },
     methods: {
         getBounds: function() {
-            fetch(`${window.location.protocol}//${window.location.host}/api/data/bounds`, {
+            this.loading = true;
+
+            let filter = '';
+            if (this.filter) {
+                filter = `&filter=${encodeURIComponent(this.filter)}`
+            }
+
+            fetch(`${window.location.protocol}//${window.location.host}/api/data/bounds?limit=100&${filter}`, {
                 method: 'GET',
                 credentials: 'same-origin'
             }).then((response) => {
                   return response.json();
             }).then((body) => {
                 this.bounds = body;
+                this.loading = false;
             });
         }
     },
