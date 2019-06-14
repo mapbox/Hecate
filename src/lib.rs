01f02c457ea1d925e03a3e5f300b8734f9731ca0
ingalls <ingalls@protonmail.com>
2019-06-14 10:29:32 -0400

    v0.69.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro, uniform_paths)]
 
-pub static VERSION: &'static str = "0.68.0";
+pub static VERSION: &'static str = "0.69.0";
 pub static POSTGRES: f64 = 10.0;
 pub static POSTGIS: f64 = 2.4;
 
