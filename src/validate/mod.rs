a5bbbdcb0759bd2a925fd22421b0ea16f3410cd4
samely <eliane@mapbox.com>
2019-07-19 12:03:02 -0400

    Cover all cases of failures for validate in unit tests

--- src/validate/mod.rs
+++ src/validate/mod.rs
@@ -57,6 +57,18 @@ mod tests {
     }
 
     #[test]
+    fn invalid_point_incomplete() {
+        match point(&String::from("70.56")){
+            Ok(_) => (),
+            Err(err) =>  assert_eq!(err.as_json(),json!({
+                "code": 400,
+                "reason": "Point must be Lng,Lat",
+                "status": "Bad Request"
+            }), "not ok - the longitude is incomplete."),
+        }
+    }
+
+    #[test]
     fn invalid_point_longitude() {
         match point(&String::from("lng,70.56")){
             Ok(_) => (),
@@ -68,13 +80,61 @@ mod tests {
         }
     }
 
+    #[test]
+    fn invalid_point_latitude() {
+        match point(&String::from("-122.44578,none.986")){
+            Ok(_) => (),
+            Err(err) =>  assert_eq!(err.as_json(),json!({
+                "code": 400,
+                "reason": "Latitude coordinate must be numeric",
+                "status": "Bad Request"
+            }), "not ok - the latitude is invalid."),
+        }
+    }
+
+    #[test]
+    fn invalid_point_longitude_out() {
+        match point(&String::from("-181.578125,-30.600094")){
+            Ok(_) => (),
+            Err(err) =>  assert_eq!(err.as_json(),json!({
+                "code": 400,
+                "reason": "Longitude exceeds bounds",
+                "status": "Bad Request"
+            }), "not ok - the longitud is out of range."),
+        }
+    }
+
+    #[test]
+    fn invalid_point_latitude_out() {
+        match point(&String::from("-107.578125,-100.600094")){
+            Ok(_) => (),
+            Err(err) =>  assert_eq!(err.as_json(),json!({
+                "code": 400,
+                "reason": "Latitude exceeds bounds",
+                "status": "Bad Request"
+            }), "not ok - the latitude is out of range."),
+        }
+    }
+
     #[test]   
     fn valid_bbox() {
         assert_eq!(bbox(&[-107.578125,-30.600094,56.162109,46.377254].to_vec()).ok(),Some(()), "ok - point coordinates is valid.");
     }
 
     #[test]
-    fn invalid_bbox_minX() {
+    fn invalid_bbox_incomplete() {
+        match bbox(&[-181.0,-30.600094,56.162109].to_vec()){
+            Ok(_) => (),
+            Err(err) =>  assert_eq!(err.as_json(),json!({
+                "code": 400,
+                "reason": "Invalid BBOX",
+                "status": "Bad Request"
+            }), "not ok - the BBOX is incomplete."),
+        }
+    }
+
+    #[test]
+    fn invalid_bbox_minx() {
         match bbox(&[-181.0,-30.600094,56.162109,46.377254].to_vec()){
             Ok(_) => (),
             Err(err) =>  assert_eq!(err.as_json(),json!({
@@ -84,6 +144,64 @@ mod tests {
             }), "not ok - the minX value is invalid."),
         }
     }
+
+    #[test]
+    fn invalid_bbox_miny() {
+        match bbox(&[-107.578125,-94.600094,56.162109,46.377254].to_vec()){
+            Ok(_) => (),
+            Err(err) =>  assert_eq!(err.as_json(),json!({
+                "code": 400,
+                "reason": "BBOX minY value must be a number between -90 and 90",
+                "status": "Bad Request"
+            }), "not ok - the minY value is invalid."),
+        }
+    }
+
+    #[test]
+    fn invalid_bbox_maxx() {
+        match bbox(&[-107.578125,-30.600094,190.162109,46.377254].to_vec()){
+            Ok(_) => (),
+            Err(err) =>  assert_eq!(err.as_json(),json!({
+                "code": 400,
+                "reason": "BBOX maxX value must be a number between -180 and 180",
+                "status": "Bad Request"
+            }), "not ok - the maxX value is invalid."),
+        }
+    }
+
+    #[test]
+    fn invalid_bbox_maxy() {
+        match bbox(&[-107.578125,-30.600094,56.162109,196.377254].to_vec()){
+            Ok(_) => (),
+            Err(err) =>  assert_eq!(err.as_json(),json!({
+                "code": 400,
+                "reason": "BBOX maxY value must be a number between -90 and 90",
+                "status": "Bad Request"
+            }), "not ok - the maxY value is invalid."),
+        }
+    }
+
+    #[test]
+    fn invalid_bbox_minx_gt_maxx() {
+        match bbox(&[107.578125,-30.600094,56.162109,46.377254].to_vec()){
+            Ok(_) => (),
+            Err(err) =>  assert_eq!(err.as_json(),json!({
+                "code": 400,
+                "reason": "BBOX minX value cannot be greater than maxX value",
+                "status": "Bad Request"
+            }), "not ok - the minX value is greater than maxX."),
+        }
+    }
+
+    #[test]
+    fn invalid_bbox_miny_gt_maxy() {
+        match bbox(&[-107.578125,30.600094,56.162109,-46.377254].to_vec()){
+            Ok(_) => (),
+            Err(err) =>  assert_eq!(err.as_json(),json!({
+                "code": 400,
+                "reason": "BBOX minY value cannot be greater than maxY value",
+                "status": "Bad Request"
+            }), "not ok - the minY value is greater than maxY."),
+        }
+    }
 }
-
-
