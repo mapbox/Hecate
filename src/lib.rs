4b2508dfb3257b0ea78cb9ce5671579f8203bbba
ingalls <ingalls@protonmail.com>
2019-12-20 11:52:58 -0400

    v0.82.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub static VERSION: &'static str = "0.82.0";
+pub static VERSION: &'static str = "0.82.1";
 pub static POSTGRES: f64 = 10.0;
 pub static POSTGIS: f64 = 2.4;
 pub static HOURS: i64 = 24;
