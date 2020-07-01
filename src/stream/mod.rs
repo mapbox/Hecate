bea11129cc8720f7335a3b867f896cd7718e2c27
Ilissa Blech <ilissa.blech@mapbox.com>
2020-07-01 13:12:46 -0400

    fix formatting

--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -23,7 +23,7 @@ impl futures::stream::Stream for PGStream {
 
     fn poll(&mut self) -> Result<futures::Async<Option<Self::Item>>, Self::Error> {
         println!("Fetching items from {}", &self.cursor);
-        println!("{Starting polling}");
+        println!("Starting polling");
         let rows = match self.trans.query(&*format!("FETCH 1000 FROM {};", &self.cursor), &[]) {
             Ok(rows) => rows,
             Err(err) => { return Err(HecateError::new(500, err.to_string(), None)); }
